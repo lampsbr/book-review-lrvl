5528f9c0f5f67816d07c28abbd2f5bcf1525b2fc
Lamps <lamparolho@gmail.com>
2025-04-10 07:02:39 -0300

    Add review scopes and fillable attributes to Book and Review models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,11 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
     use HasFactory;
+
+    //lazy loads the relation
     public function reviews() {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder {
+        return $query->where("title","like","%$title%");
+    }
+
+    public function scopePopular(Builder $query) : Builder{
+        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRating(Builder $query) : Builder {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
