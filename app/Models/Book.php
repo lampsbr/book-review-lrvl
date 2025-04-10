8f85761643f0decfa610616d5a8eaf98e959284e
Lamps <lamparolho@gmail.com>
2025-04-10 09:46:45 -0300

    Implement BookController with resource routes and search functionality for books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,25 +5,52 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as Querybuilder;
 
 class Book extends Model
 {
     use HasFactory;
 
     //lazy loads the relation
-    public function reviews() {
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
 
-    public function scopeTitle(Builder $query, string $title): Builder {
-        return $query->where("title","like","%$title%");
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where("title", "like", "%$title%");
     }
 
-    public function scopePopular(Builder $query) : Builder{
-        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query
+            ->withCount([
+                'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ])
+            ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRating(Builder $query) : Builder {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+    public function scopeHighestRating(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, $minReviews): Builder|Querybuilder {
+        //use `having`for querying the return of aggregate functions. `Where wouldn't work in here. You could use subqueries too.
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $q, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $q->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $q->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $q->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BookController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource(('books'), BookController::class);
